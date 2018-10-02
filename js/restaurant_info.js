3e62ca88f80325906409f846c30891e7e779dd06
Sama Mushtaq  <sm9wani@gmail.com>
2018-10-01 23:17:47 -0500

    added alt attribute to restaurant imgs on index.html

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: 'pk.eyJ1Ijoic213aWxsc29uIiwiYSI6ImNqbWh0bWp0ZjBydDUza211cXo3NXNidG0ifQ.06QDRZXuLAiihfgmBsReaw',
+        mapboxToken:'pk.eyJ1Ijoic213aWxsc29uIiwiYSI6ImNqbWh0bWp0ZjBydDUza211cXo3NXNidG0ifQ.06QDRZXuLAiihfgmBsReaw',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
